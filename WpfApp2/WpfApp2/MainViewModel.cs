1a4733b900ea807da53005bfe3b6134d00a27ab5
pixel-testzero <accsteamtest8@gmail.com>
2026-03-12 22:27:08 +0300

    Add files via upload

---
+++ WpfApp2/WpfApp2/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp2
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _textValue = "Начальное значение";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string TextValue
+        {
+            get { return _textValue; }
+            set
+            {
+                if (_textValue != value)
+                {
+                    _textValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        // Команда для изменения значения через ViewModel
+        private RelayCommand _changeTextCommand;
+        public RelayCommand ChangeTextCommand
+        {
+            get
+            {
+                return _changeTextCommand ??= new RelayCommand(obj =>
+                {
+                    TextValue = "Изменено из ViewModel: " + DateTime.Now.ToString("HH:mm:ss");
+                });
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using WpfApp2;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Устанавливаем контекст данных
+            DataContext = new MainViewModel();
+        }
+    }
+}
